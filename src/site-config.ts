0427a74a2feda887687d0664b5927b85adf67678
fmerg@protonmail.com <fmerg@protonmail.com>
2026-01-20 20:51:07 +0200

    Apply breadcrumb mechanism

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,7 +1,15 @@
 const sample  = "foo";
 const todo    = "TODO";
 
+const firstName   = "Κωνσταντίνα";
+const lastName    = "Ζερβάκη";
+const fullName    = `${firstName} ${lastName}`;
+
+const profession  = "Ψυχολόγος";
+
 export const CONFIG = {
   sample,
   todo,
+  fullName,
+  profession,
 }
