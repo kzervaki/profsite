b5872b981d7a27b9df4672f325b846239096f1d9
fmerg@protonmail.com <fmerg@protonmail.com>
2026-01-21 15:45:50 +0200

    Implement site footer
    - sticky on non-mobile screen
    - no margins
    - contact icons

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,21 +1,25 @@
-const sample  = "foo";
-const todo    = "TODO";
-
-const firstName   = "Κωνσταντίνα";
-const lastName    = "Ζερβάκη";
-const fullName    = `${firstName} ${lastName}`;
-
-const profession  = "Ψυχολόγος";
-
-const siteTitle     = fullName;
-
-const siteSubtitle  = "Ψυχολόγος";
+const siteTitle           = "Κωνσταντίνα Ζερβάκη";
+const siteSubtitle        = "Ψυχολόγος";
+const year                = "2026";
+const fullName            = "Κωνσταντίνα Ζερβάκη";
+const profession          = "Ψυχολόγος";
+const professionTitle     = "Ψυχολόγος ΜΑ";
+const specialtySystemic   = "Συστημική ψυχοθεραπεύτρια";
+const specialtyDialectic  = "Διαλεκτική συμπεριφορική θεραπεύτρια";
+const phone               = "+30 6987418349 ";
+const email               = "zervkons@gmail.com";
+const address             = "Λουίζης Ριανκούρ 64 (Πύργος Απόλλων), Πανόρμου, ΤΚ: 11523";
 
 export const CONFIG = {
-  sample,
-  todo,
-  fullName,
-  profession,
   siteTitle,
   siteSubtitle,
+  year,
+  fullName,
+  profession,
+  professionTitle,
+  specialtySystemic,
+  specialtyDialectic,
+  phone,
+  email,
+  address,
 }
