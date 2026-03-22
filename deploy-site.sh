225019c43424ceab526b43a639596dd72deb00ee
fmerg@protonmail.com <fmerg@protonmail.com>
2026-03-22 09:35:30 +0200

    Make changes for deploying under custom domain

--- deploy-site.sh
+++ deploy-site.sh
@@ -2,8 +2,23 @@
 
 git checkout deployment
 git rebase master
+git reset --hard master
 npm run build
+# For deploying under custom domain
+find docs/ -name "*.html" -type f -exec sed -i \
+  -e 's|href="/profsite/styles/|href="/styles/|g' \
+  -e 's|href="/profsite/images/|href="/images/|g' \
+  -e 's|src="/profsite/images/|src="/images/|g' \
+  -e 's|href="/profsite"|href="/"|g' \
+  -e 's|href="/profsite/|href="/|g' \
+  -e 's|src="/profsite/|src="/|g' \
+  -e 's|action="/profsite|action="/|g' \
+  -e 's|a="/profsite"|a="/"|g' \
+  -e 's|`/profsite|`/|g' \
+  -e 's|"/profsite|"/|g' \
+  -e 's|/profsite/|/|g' \
+  {} \;
 git add docs
 git commit -m "Generate artifacts"
-git push origin deployment --force-with-lease
+git push origin deployment --force
 git checkout -
