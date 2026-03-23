daa47aa4d386ca4353442b372bd56aa77ad0ac15
fmerg@protonmail.com <fmerg@protonmail.com>
2026-03-23 21:57:20 +0200

    optimization: Apply prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,10 @@ export default defineConfig({
   site: 'https://kzervaki.github.io',
   base: '/profsite',
   outDir: 'docs',
+  prefetch: {
+    prefetch: true, // enable prefetching
+    defaultStrategy: 'hover', // 'tap' | 'hover' | 'viewport'
+    throttle: 3,  // throttle prefetch requests
+    prefetchAll: false, // prefetch all links by default
+  },
 });
